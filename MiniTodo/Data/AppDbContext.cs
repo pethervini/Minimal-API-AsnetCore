3e7dd7dfe0bca5e57809dbd15e447f4507dcc8f5
Petherson Vinicius da Silva Amorin <pethersonamorim@bne.com.br>
2022-12-30 19:21:00 -0300

    Adicionar arquivos de projeto.

---
+++ MiniTodo/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniTodo.Models;
+
+namespace MiniTodo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlite("DataSource=app.db;Cache=Shared");
+    }
+}
